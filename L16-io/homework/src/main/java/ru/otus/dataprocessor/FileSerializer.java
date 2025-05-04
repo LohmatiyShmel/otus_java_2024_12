67d844a852dc4e5ca3488768cde77f6435f2fbd8
LohmatiyShmel <131897080+LohmatiyShmel@users.noreply.github.com>
2025-05-04 15:14:35 +0300

    hw16 - сериализация

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,51 @@
 package ru.otus.dataprocessor;
 
-import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import ru.otus.model.Measurement;
 
 public class FileSerializer implements Serializer {
+    private final String fileName;
 
-    public FileSerializer(String fileName) {}
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
-    public void serialize(Map<String, Double> data) {
+    public void serialize(final Map<String, Double> data) {
         // формирует результирующий json и сохраняет его в файл
+        final List<Measurement> measurements = new ArrayList<>();
+        data.forEach((name, value) -> measurements.add(new Measurement(name, value)));
+        measurements.sort(Comparator.comparing(Measurement::name));
+        String json = buildJson(measurements);
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(json);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
+    }
+
+    private String buildJson(final List<Measurement> measurements) {
+        final StringBuilder sb = new StringBuilder("{");
+
+        for (int i = 0; i < measurements.size(); i++) {
+            Measurement m = measurements.get(i);
+            sb.append("\"").append(escapeJson(m.name())).append("\":").append(m.value());
+
+            if (i < measurements.size() - 1) {
+                sb.append(",");
+            }
+        }
+        return sb.append("}").toString();
+    }
+
+    private String escapeJson(final String input) {
+        return input.replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
     }
 }
--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -1,6 +1,6 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import ru.otus.model.Measurement;
@@ -8,8 +8,19 @@ import ru.otus.model.Measurement;
 public class ProcessorAggregator implements Processor {
 
     @Override
-    public Map<String, Double> process(List<Measurement> data) {
+    public Map<String, Double> process(final List<Measurement> data) {
         // группирует выходящий список по name, при этом суммирует поля value
-        return Collections.emptyMap();
+        if (data.isEmpty()) {
+            throw new FileProcessException("Dataset is empty");
+        }
+        final Map<String, Double> summedValues = new HashMap<>();
+        for (Measurement measurement : data) {
+            if (summedValues.get(measurement.name()) != null) {
+                summedValues.put(measurement.name(), summedValues.get(measurement.name()) + measurement.value());
+                continue;
+            }
+            summedValues.put(measurement.name(), measurement.value());
+        }
+        return summedValues;
     }
 }
