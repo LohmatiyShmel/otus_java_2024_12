262189efcfd2feaab34e9565fcc32a1312934537
LohmatiyShmel <131897080+LohmatiyShmel@users.noreply.github.com>
2025-07-15 21:03:53 +0300

    HW32 - Реализация SensorDataProcessorBuffered

--- L32-concurrentCollections/QueueDemo/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ L32-concurrentCollections/QueueDemo/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -1,5 +1,10 @@
 package ru.otus.services.processors;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.api.SensorDataProcessor;
@@ -13,6 +18,8 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final List<SensorData> dataBuffer = new ArrayList<>();
+    private final ReentrantLock lock = new ReentrantLock();
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
@@ -21,18 +28,30 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     @Override
     public void process(SensorData data) {
-        /*
+        lock.lock();
+        try {
+            dataBuffer.add(data);
             if (dataBuffer.size() >= bufferSize) {
                 flush();
             }
-        */
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void flush() {
+        lock.lock();
         try {
-            // writer.writeBufferedData(bufferedData);
+            if (!dataBuffer.isEmpty()) {
+                List<SensorData> dataToWrite = new ArrayList<>(dataBuffer);
+                Collections.sort(dataToWrite, Comparator.comparing(SensorData::getMeasurementTime));
+                writer.writeBufferedData(dataToWrite);
+                dataBuffer.clear();
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
+        } finally {
+            lock.unlock();
         }
     }
 
